3934b220399f195f3069add4afbb26cdc74e73fa
AbdulrehmanSuliman <55790261+AbdulrehmanSuliman@users.noreply.github.com>
2022-03-01 00:17:59 +0200

    initializing react app and prepare folders

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
