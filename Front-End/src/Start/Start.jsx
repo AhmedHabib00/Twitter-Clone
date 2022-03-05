f7940aeb071938b0ab96a8bda68f2011a159a32a
AbdulrehmanSuliman <55790261+AbdulrehmanSuliman@users.noreply.github.com>
2022-03-05 03:09:12 +0200

    adding sign up and sign in buttons in start page

--- Front-End/src/Start/Start.jsx
+++ Front-End/src/Start/Start.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Start.css';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import Button from '@mui/material/Button';
 
 function Start() {
   return (
@@ -9,8 +10,47 @@ function Start() {
         <div className="right-group">
           <TwitterIcon className="right-logo" />
           <h1>Happening now</h1>
-          <h2>Join Twitter today</h2>
-          <button type="submit">Sign up with email</button>
+          <h2>Join Whisper today.</h2>
+          <div className="buttons-group">
+            <Button
+              variant="outlined"
+              className="signup-tweet-google"
+            >
+              <img
+                className="google-logo"
+                src="https://img.icons8.com/color/48/000000/google-logo.png"
+                alt="google logo"
+              />
+              Sign up with Google
+            </Button>
+            <Button
+              variant="outlined"
+              className="signup-tweet"
+            >
+              Sign up with email
+            </Button>
+            <p>
+              By signing up, you agree to the
+              {' '}
+              <span>Terms of Service</span>
+              {' '}
+              and
+              {' '}
+              <span>Privacy Policy</span>
+              ,
+              including
+              {' '}
+              <span>Cookie Use</span>
+              .
+            </p>
+            <h3>Already have an acount?</h3>
+            <Button
+              variant="outlined"
+              className="sign-in"
+            >
+              Sign in
+            </Button>
+          </div>
         </div>
       </div>
       <div className="left-column-container">
