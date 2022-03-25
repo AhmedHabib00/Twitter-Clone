5b02ae7383b7cf56f7c175dd865d6bfd2e9f0a3c
nohaelboghdady <nohaelboghdady12@gmail.com>
2022-03-25 19:25:11 +0200

    managed feed

--- Front-End/src/Home/Components/Feed.jsx
+++ Front-End/src/Home/Components/Feed.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Feed.module.css';
 import Post from './Post';
 
-function Feed() {
+function Feed({ data }) {
   return (
     <div className={styles.feed}>
-      <Post />
+
+      {
+        data && data.map((post) => (
+          <Post
+            id={post.id}
+            displayname={post.displayname}
+            username={post.username}
+            content={post.content}
+            img1={post.img1}
+            img2={post.img2}
+            img3={post.img3}
+            img4={post.img4}
+          />
+        ))
+    }
+
     </div>
   );
 }
 
+Feed.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    displayname: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    img1: PropTypes.string.isRequired,
+    img2: PropTypes.string.isRequired,
+    img3: PropTypes.string.isRequired,
+    img4: PropTypes.string.isRequired,
+
+    // images: PropTypes.arrayOf(PropTypes.shape({
+    //   id: PropTypes.number.isRequired, src: PropTypes.string.isRequired,
+    // })).isRequired,
+  })).isRequired,
+};
 export default Feed;
--- Front-End/src/Home/Home.jsx
+++ Front-End/src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Feed from './Components/Feed';
 import styles from './Home.module.css';
 import TweetBox from './Components/TweetBox';
+import Data from './Components/PostData.json';
 
 /**
  * consists of 3 main components.
@@ -13,9 +14,15 @@ function Home() {
       <div className={styles['home-header']}>
         <h2>Home</h2>
       </div>
+<<<<<<< HEAD
       <TweetBox />
       <hr className={styles['home-hor-hr']} />
       <Feed className={styles.feed} />
+=======
+      <TweetBox className="home-tweetbox" />
+      <hr className="home-hor-hr" />
+      <Feed className="feed" data={Data} />
+>>>>>>> f0fbe1c... managed feed
     </div>
   );
 }
