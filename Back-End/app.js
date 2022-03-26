185db6b77467824503ce42e767684e5907b68de3
Mariam85 <91765187+Mariam85@users.noreply.github.com>
2022-03-26 12:20:22 +0200

    Update app.js
    added app.use for multer
    added app.use for tweets route

--- Back-End/app.js
+++ Back-End/app.js
@@ -2,12 +2,16 @@ const express=require('express');
 const bodyParser= require('body-parser');
 const session = require('express-session');
 const mongoose= require('./database');
-const port = process.env.PORT || 3000;
+const port = 3000;
 const app = express();
-
+app.use('/uploads',express.static('uploads'));
 
 
 
 app.use(bodyParser.urlencoded({extended : true})) //for body-parser to return warning
 const server =app.listen(port,()=>
 console.log(`app is running on port ${port}`));
+
+
+const postRoute = require('./components/Tweets/tweets');
+app.use("/tweets", postRoute);
