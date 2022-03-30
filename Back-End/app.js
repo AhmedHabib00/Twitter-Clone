e7d99e92b23360a3639e4df7b09900a760d3f277
Mariam85 <91765187+Mariam85@users.noreply.github.com>
2022-03-30 12:10:43 +0200

    used bodyParser.json() for body parameters

--- Back-End/app.js
+++ Back-End/app.js
@@ -4,16 +4,20 @@ const session = require('express-session');
 const mongoose= require('./database');
 const port = 3000;
 const app = express();
+app.use(bodyParser.urlencoded({extended : true})) //for body-parser to return warning
+app.use(bodyParser.json())
 app.use('/uploads',express.static('uploads'));
 
 
 
-app.use(bodyParser.urlencoded({extended : true})) //for body-parser to return warning
+
 const server =app.listen(port,()=>
 console.log(`app is running on port ${port}`));
 
 
 const postRoute = require('./components/Tweets/tweets');
+
 app.use("/tweets", postRoute);
+
 
 module.exports = server
