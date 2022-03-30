b9b1961d50d30303edd425819d76b358e925caf4
nohaelboghdady <nohaelboghdady12@gmail.com>
2022-03-30 22:45:32 +0200

    testing

--- Front-End/src/Home/Components/ImagePopUp.jsx
+++ Front-End/src/Home/Components/ImagePopUp.jsx
@@ -3,9 +3,18 @@ import CloseIcon from '@mui/icons-material/Close';
 import PropTypes from 'prop-types';
 import styles from './ImagePopUp.module.css';
 
+/**
+ *
+ * @param {boolean} trigger
+ * @param {function} setTrigger
+ * @param {elements} childern
+ *
+ * @returns div element which will contain popUp including close-icon
+ * and passed childern
+ */
 function ImagePopUp({ trigger, setTrigger, children }) {
   return (trigger) ? (
-    <div className={styles.popup}>
+    <div data-testid="image-render-test" className={styles.popup}>
       <div className={styles['popup-inner']}>
         <CloseIcon className={styles['close-btn']} onClick={() => { setTrigger(false); }} />
         {children}
