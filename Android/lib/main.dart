59347a3f7d23faf14c543d444337ed2b71417093
Ahmed Gamal <amgamal200111@gmail.com>
2022-03-31 23:36:42 +0200

    Attemtping to solve conflicts

--- Android/lib/main.dart
+++ Android/lib/main.dart
@@ -6,27 +6,16 @@ import 'package:whisper/shared/bloc_observer.dart';
 
 import 'layout/profile_layout.dart';
 
-void main() {
-  BlocOverrides.runZoned(
-        () {
-      // Use cubits...
-    },
-    blocObserver: MyBlocObserver(),
-  );
-
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return const MaterialApp(
+      title: 'Whisper',
       debugShowCheckedModeBanner: false,
-      home: ProfilePage(),
+      home: HomePage(),
     );
   }
 }
