678ecdbeb3ada1c717d94caf34dcf454ab7b0be0
EizaldinTarik <62310240+EizaldinTarik@users.noreply.github.com>
2022-03-31 21:14:43 +0200

    Update main.dart
    import file was redirected to Home.dart instead of home_page.dart and demo class is removed

--- Android/lib/main.dart
+++ Android/lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:whisper/layout/home_page.dart';
+import 'package:whisper/layout/Home.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
