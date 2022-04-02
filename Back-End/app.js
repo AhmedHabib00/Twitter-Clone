4f526b3b072d9372a0002ef7c3c7e4a9c5d53e95
Abuelfadl <omar.abuelfadl99@eng-st.cu.edu.eg>
2022-04-03 00:13:51 +0200

    New branch with all of the changes

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,13 +1,22 @@
-const express=require('express');
-const bodyParser= require('body-parser');
-const session = require('express-session');
-const mongoose= require('./database');
-const port = process.env.PORT || 3000;
+const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
+const databse = require('./database');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 
+const userProfileRoute = require('./components/UserProfile/userProfile')
 
+app.use('/user', userProfileRoute);
 
-app.use(bodyParser.urlencoded({extended : true})) //for body-parser to return warning
-const server =app.listen(port,()=>
-console.log(`app is running on port ${port}`));
+app.listen(3000,function(){
+    console.log("Server is up and running on port 3000");
+});
+
+module.exports = app;
