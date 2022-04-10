8671677f04ded89093222f35addfb8dc4fc232e0
Abdulrehman Suliman <abdulrehman.kassem00@eng-st.cu.edu.eg>
2022-04-10 17:05:00 +0200

    authorization

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -19,17 +19,12 @@ import Search from './Search/Search';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setisAdmin] = useState(false);
-  localStorage.clear();
+  // localStorage.clear();
   useEffect(() => {
-<<<<<<< HEAD
     const logged = localStorage.getItem('logged');
     const admin = localStorage.getItem('admin');
     setIsLoggedIn(logged);
     setisAdmin(!admin);
-=======
-    setIsLoggedIn(true);
-    setisAdmin(false);
->>>>>>> d576aba0a0d1f33f13c25a06a32bf6894085a430
   }, []);
   const mainPage = () => {
     if (isLoggedIn) {
