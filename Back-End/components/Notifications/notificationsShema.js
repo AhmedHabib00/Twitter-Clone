0f44d7657a2eb20c6e95fb1a6a129be57b1a43bf
mohdfarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2022-04-11 12:55:50 +0200

    Restore users branch

--- Back-End/components/Notifications/notificationsShema.js
+++ Back-End/components/Notifications/notificationsShema.js
@@ -6,10 +6,6 @@ const notificationsSchema = new mongoose.Schema ({
     reason: {type: String}, //indicates notification type (fro reply /retweet/ like)
     from: [{type:Schema.Types.ObjectId, ref: 'User' }],
     to: [{type:Schema.Types.ObjectId, ref: 'User' }]
-   
-
-
-
 
 },{timestamps: true});
 const Notification= mongoose.model('Notification',userSchema);
