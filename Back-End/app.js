aef6182c1f067cac6582bc00bcafc8bd67eaa5fe
NouranFoda <nouran2000.foda@gmail.com>
2022-04-14 01:57:28 +0200

    removed console logs in sendEmail

--- Back-End/app.js
+++ Back-End/app.js
@@ -12,30 +12,14 @@ const signUp = require('./components/Auth/signupRoute');
 const login = require('./components/Auth/loginRoute');
 const oAuth = require('./components/Auth/OAuthRoute');
 const forgotPassword = require('./components/Auth/forgotPasswordRoute');
-
-
-
 // Users  
 var users = require('./components/User/user');
-
-
-
-
-app.use('/uploads',express.static('uploads'));
-
-
 //user profile 
 const userProfileRoute = require('./components/UserProfile/userProfile');
-
-
-
-
 //tweets 
 const postRoute = require('./components/Tweets/tweets');
-
-
-
-
+//Admins
+const admins = require('./components/Admins/admin')
 
 
 
@@ -47,18 +31,16 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use('/uploads',express.static('uploads'));
 
 // Users endpoints
 app.use('/users', users);
 
-
 //User Profile endpoints
 app.use('/user', userProfileRoute);
 
-
 //tweets endpoints
 app.use("/tweets", postRoute);
-
 
 //signUp and auth endpoints
 app.use ('/signUp',signUp);
@@ -66,15 +48,14 @@ app.use ('/login',login);
 app.use ('/auth',oAuth);
 app.use ('/forgotPassword',forgotPassword);
 
-app.use(bodyParser.json());       // To support JSON-encoded bodies
-app.use(bodyParser.urlencoded({extended : true})) // For body-parser to return warning
-
 // Test
 app.use('/__test__',express.static('__test__'));
 
 // Admins end points
-const admins = require('./components/Admins/admin')
 app.use("/admins", admins)
+
+
+
 
 const server = app.listen(port,()=>
     console.log(`app is running on port ${port}`));
