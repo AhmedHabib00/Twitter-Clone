fca1a3ffb79d62161ce058e63f550f844ed13f65
nohaelboghdady <nohaelboghdady12@gmail.com>
2022-04-18 02:32:17 +0200

    migrated get request to postServices

--- Front-End/src/Home/Home.jsx
+++ Front-End/src/Home/Home.jsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Feed from './Components/Feed';
 import styles from './Home.module.css';
 import TweetBox from './Components/TweetBox';
+import GetPostsArray from '../Services/postServices';
 
 function Home() {
   const [postData, setPostData] = useState();
   // const [notificationData, setNotificationData] = useState();
 
   useEffect(() => {
-    axios.get('http://localhost:8000/posts')
-      .then((resp) => {
+    (async () => {
+      const resp = await GetPostsArray();
+      if (resp.status === 200) {
         setPostData(resp.data);
-      }).catch((error) => {
-        console.log(error);
-      });
+      }
+    })();
   }, []);
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:8000/posts')
+  //     .then((resp) => {
+  //       setPostData(resp.data);
+  //     }).catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
 
   return (
     <div className={styles.home}>
