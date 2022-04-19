340a4900a6a5c60a7e891d4ffd86b48a28af009e
Asmaa Lotfy <asmaalotfy2000@gmail.com>
2022-04-19 21:46:56 +0200

    initial 2

--- Front-End/src/Home/Home.jsx
+++ Front-End/src/Home/Home.jsx
@@ -1,29 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Feed from './Components/Feed';
+// import styles from './Home.module.css';
+// import TweetBox from './Components/TweetBox';
+
+// function Home() {
+//   const [data, setData] = useState();
+
+//   useEffect(() => {
+//     axios.get('http://localhost:8000/posts')
+//       .then((resp) => {
+//         setData(resp.data);
+//       }).catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+
+//   return (
+//     <div className={styles.home}>
+//       <div className={styles['home-header']}>
+//         <h2>Home</h2>
+//       </div>
+//       <TweetBox placeHolder="What's happening" boxId="home" />
+//       <hr className={styles['home-hor-hr']} />
+//       <Feed className={styles.feed} data={data} />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+import React from 'react';
 import Feed from './Components/Feed';
 import styles from './Home.module.css';
 import TweetBox from './Components/TweetBox';
+import Data from './Components/PostData.json';
 
+/**
+ * consists of 3 main components.
+ * Header, Tweetbox, Feed
+ */
 function Home() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/posts')
-      .then((resp) => {
-        setData(resp.data);
-      }).catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <div className={styles.home}>
       <div className={styles['home-header']}>
         <h2>Home</h2>
       </div>
-      <TweetBox placeHolder="What's happening" boxId="home" />
+      <TweetBox />
       <hr className={styles['home-hor-hr']} />
-      <Feed className={styles.feed} data={data} />
+      <Feed className={styles.feed} data={Data} />
     </div>
   );
 }
