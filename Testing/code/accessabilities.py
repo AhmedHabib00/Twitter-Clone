26cc0376ca1f5d67e35d217d8b0ba7732695725f
Ahmed Habib <73848497+AhmedHabib00@users.noreply.github.com>
2022-04-23 02:25:02 +0200

    Update accessabilities.py

--- Testing/code/accessabilities.py
+++ Testing/code/accessabilities.py
@@ -9,4 +9,4 @@ signup_close_button = '#signup-modals > div > div > div:nth-child(1) > button'
 signup_name_error = '#signup-modals > div > form > div.StepOne_body__qklkQ > p'
 signup_email_error = '#signup-modals > div > form > div.StepOne_body__qklkQ > label:nth-child(3) > p'
 
-chrome_driver = 'E:\Sem 6\Software Engineering\phase_3\drivers\chromedriver.exe'
+chrome_driver = '../drivers/chromedriver.exe'
