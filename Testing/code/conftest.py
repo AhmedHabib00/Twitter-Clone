be2fa7ed2da13814a66e334ad1c9888eb27241b9
AhmedHabib00 <ahmed.habib98@eng-st.cu.edu.eg>
2022-04-24 03:20:30 +0200

    fix test chromedriver

--- Testing/code/conftest.py
+++ Testing/code/conftest.py
@@ -13,7 +13,8 @@ def driver_setup():
     chrome_options = Options()
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    # chrome_options.headless = True
+
+
     driver = webdriver.Chrome(service=s, options=chrome_options, desired_capabilities=caps)
     driver.implicitly_wait(20)
     driver.maximize_window()
