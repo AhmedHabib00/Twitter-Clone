a21e782c60d303a5444e25bbb488e35aa1f76b81
AhmedHabib00 <ahmed.habib98@eng-st.cu.edu.eg>
2022-04-24 03:44:33 +0200

    fix test chromedriver

--- Testing/code/conftest.py
+++ Testing/code/conftest.py
@@ -13,8 +13,9 @@ def driver_setup():
     chrome_options = Options()
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=s, options=chrome_options, desired_capabilities=caps)
     driver.implicitly_wait(20)
     driver.maximize_window()
