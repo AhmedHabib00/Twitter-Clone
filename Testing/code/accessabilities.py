67176ffde87d822b0fb635b67610c7761d96df34
yasmiinezaki <71704853+yasmiinezaki@users.noreply.github.com>
2022-04-26 21:07:57 +0200

    Update accessabilities.py

--- Testing/code/accessabilities.py
+++ Testing/code/accessabilities.py
@@ -54,8 +54,7 @@ image_4_css_selector = "img[src$='imagenopq4.png']"
 image_5_css_selector = "img[src$='imagerstu5.png']"
 images_in_tweetbox_id = 'image-box-image-canvas'
 
-# chrome_driver = '/chromedriver/chromedriver'
-chrome_driver = 'E:\Sem 6\Software Engineering\phase-3\drivers\chromedriver.exe'
+chrome_driver = '/chromedriver/chromedriver'
 email = 'testingwhisper2@gmail.com'
 password = 'BnSuliman@1'
 password_1 = 'whispertest2'
