d0394761882ef7f057e7e3bce4c67d2476789040
Yasmeen Zaki Bassyouny Nossair <yasmeen_zaki@stud.cu.edu.eg>
2022-04-26 22:11:23 +0200

    adding android testing

---
+++ Testing/android code/and_accessabilities.py
@@ -0,0 +1,37 @@
+#SIGN UP
+testing_google_mail="testingwhisper2@gmail.com"
+testing_google_password="whispertest2"
+
+#START PAGE
+username='habib'
+password='7abib7ABIB@'
+sign_in_button_id = 'Login'
+sign_up_button_id = 'Sign Up'
+Start_page_id = "See what's happening in the world right now"
+#SIGN IN PAGE
+login_page_xpath='//android.view.View[@content-desc="Login"]'
+login_button_xpath='//android.widget.Button[@content-desc="Login"]'
+login_mail_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]'
+login_password_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]'
+login_signup_option_id='Sign up'
+login_forget_password_id='Forgot Password?'
+login_with_google_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[2]'
+login_with_facebook_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[1]'
+login_mail_error_id='Enter correct email'
+login_password_error_id='Enter correct Password'
+login_back_button_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button'
+
+#SIGN UP PAGE
+signup_page_id='Sign Up'
+signup_mail_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.EditText[1]'
+signup_password_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.EditText[2]'
+signup_date_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[3]'
+signup_next_button_id='Next'
+signup_login_option_id='Login'
+signup_with_google_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.ImageView[2]'
+signup_with_facebook_xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.widget.ImageView[1]'
+
+#Profile_Page
+profile_edit_xpath=''
+profile_change_cover_xpath=''
+
---
+++ Testing/android code/and_test_profile.py
@@ -0,0 +1,29 @@
+import time
+from selenium.common.exceptions import NoSuchElementException
+import and_accessabilities
+import and_conftest
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions import interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+
+def test_change_profile_pic(WebDriver):
+    try:
+        WebDriver.find_element(by=AppiumBy.XPATH, value=and_accessabilities.profile_edit_xpath).click()
+    except NoSuchElementException:
+        pass
+
+    test = False
+
+    try:
+        test = (WebDriver.find_element(by=AppiumBy.XPATH,
+                                       value=and_accessabilities.profile_change_cover_xpath).is_displayed())
+    except NoSuchElementException:
+        pass
+
+    print("Profile Edit")
+    if test:
+        print('Test Failed')
+    else:
+        print('Test Successful')
