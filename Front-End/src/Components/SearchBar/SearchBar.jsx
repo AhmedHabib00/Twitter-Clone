fd2059b1c3faac852d6e2990b44828119dd48391
Amr Zayed <amrzayed40@gmail.com>
2022-05-17 02:19:48 +0300

    adding control delay to searchbar

--- Front-End/src/Components/SearchBar/SearchBar.jsx
+++ Front-End/src/Components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import styles from './SearchBar.module.css';
@@ -8,15 +8,38 @@ import styles from './SearchBar.module.css';
  * @param {string} searchValue function passed from parent that gets the searched
  * value on value change
  */
-function SearchBar({ placeHolder, searchValue }) {
+function SearchBar({
+  placeHolder, searchValue, delay, enableDelay,
+}) {
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => { if (enableDelay) { searchValue(query); } }, delay);
+    return () => clearTimeout(timeOutId);
+  }, [query]);
+
   const updateText = () => {
     const { value } = document.getElementsByClassName(styles['searchbar-input'])[0];
-    searchValue(value);
+    setQuery(value);
+  };
+  const EnterPressed = () => {
+    searchValue(query);
   };
   return (
     <div className={styles.searchbar}>
       <SearchOutlinedIcon className={styles['searchbar-icon']} />
-      <input id="searchbar-input-element" type="search" placeholder={placeHolder} className={styles['searchbar-input']} onChange={updateText} />
+      <input
+        id="searchbar-input-element"
+        type="search"
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            EnterPressed();
+          }
+        }}
+        placeholder={placeHolder}
+        className={styles['searchbar-input']}
+        onChange={updateText}
+      />
     </div>
   );
 }
@@ -24,5 +47,12 @@ function SearchBar({ placeHolder, searchValue }) {
 SearchBar.propTypes = {
   placeHolder: PropTypes.string.isRequired,
   searchValue: PropTypes.func.isRequired,
+  delay: PropTypes.number,
+  enableDelay: PropTypes.bool,
+};
+
+SearchBar.defaultProps = {
+  delay: 10,
+  enableDelay: true,
 };
 export default SearchBar;
