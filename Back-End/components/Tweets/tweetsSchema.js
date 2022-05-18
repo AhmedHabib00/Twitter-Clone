3c77ea2ec752e0de5f7d2f94f5a761449335daac
Omar Abuelfadl <55900407+Abuelfadl@users.noreply.github.com>
2022-05-18 04:32:24 +0200

    Update tweetsSchema.js

--- Back-End/components/Tweets/tweetsSchema.js
+++ Back-End/components/Tweets/tweetsSchema.js
@@ -1,10 +1,27 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const tweetSchema = new mongoose.Schema ({
+const tweetSchema = new mongoose.Schema({
 
-    
-
-
+    content: {type: String ,trim: true},
+    //private: {type: Boolean, default: false}  
+    pinned: {type: Boolean},
+    postedBy: { type: Schema.Types.ObjectId, ref:'User'},
+    //hashtag: {type:String},
+    //bookmarked boolean
+    likes: [{ type: Schema.Types.ObjectId ,ref:'User'}],
+    retweeters: [{ type: Schema.Types.ObjectId ,ref: 'User'}],
+    retweetInfo: [{type:Schema.Types.ObjectId, ref: 'Tweet' }],
+    replyTo: [{type:Schema.Types.ObjectId, ref: 'Tweet' }], 
+    gifs:{type:String}, //maximum of 1 gif only is allowed 
+    media: [{
+        type: String
+    }],
+    numberLikes:{type: Number},
+    numberReplies:{type: Number}, //number of DIRECT REPLIES.
+    numberRetweets:{type: Number},
+    replyingUsers:[{ type: Schema.Types.ObjectId ,ref:'User'}], //users the current user is replying to.
 },{timestamps: true});
-const Tweet= mongoose.model('Tweet',userSchema);
+
+const Tweet= mongoose.model('Tweet',tweetSchema);
 module.exports = Tweet;
