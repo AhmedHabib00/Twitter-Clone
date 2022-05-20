c009bbee781cceda0e3fcb23d7a8614419438d90
NouranFoda <nouran2000.foda@gmail.com>
2022-05-20 23:11:15 +0200

    Editing get profile info route

--- Back-End/components/UserProfile/userProfile.js
+++ Back-End/components/UserProfile/userProfile.js
@@ -93,6 +93,15 @@ router.get('/:userProfileId/profile_settings', async(req,res) => {
         const birthDate = user.birthdate;
         const profilePicture = user.profilePic;
         const coverPhoto = user.coverPhoto;
+
+        var today = new Date();
+        var birthDate2 = new Date("1995-04-14T00:00:00.000Z");
+        var age = today.getFullYear() - birthDate2.getFullYear();
+        var m = today.getMonth() - birthDate2.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate2.getDate())) 
+        {
+            age--;
+        }
         //console.log(name,location)
         //const token = jwt.sign({ _id: user.userProfileId})
         return res.status(200).send({
@@ -102,7 +111,10 @@ router.get('/:userProfileId/profile_settings', async(req,res) => {
             "description":description,
             "Birthdate":birthDate,
             "Profile Picture":profilePicture,
-            "Cover Photo": coverPhoto
+            "Cover Photo": coverPhoto,
+            "createdAt":user.createdAt.toDateString(),
+            "email":user.email,
+            "age":age
         });
     }
     catch (err){
