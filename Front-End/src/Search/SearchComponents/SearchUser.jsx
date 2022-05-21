d311979b3cf8c71040e3d2da597ed4a0afedff63
Abdulrehman Suliman <abdulrehman.kassem00@eng-st.cu.edu.eg>
2022-05-21 02:17:26 +0200

    settings done

--- Front-End/src/Search/SearchComponents/SearchUser.jsx
+++ Front-End/src/Search/SearchComponents/SearchUser.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import styles from './SearchUser.module.css';
 // import ReactRoundedImage from "react-rounded-image";
 // import MyPhoto from "./images/me.jpg";
+import { getFollowing, Follow, unFollow } from '../../Services/UserServices';
 
 /**
  * @param {Number} profileid      Id
@@ -19,14 +20,18 @@ import styles from './SearchUser.module.css';
 
 function SearchUser({
   name, username, description, profilePic, buttonStyle, id,
-  buttonStyleClicked, isButtonActive,
+  buttonStyleClicked,
 }) {
-  const [isButtonClicked, setIsButtonClicked] = useState(isButtonActive);
+  // const [isButtonClicked, setIsButtonClicked] = useState(isButtonActive);
   const [follow, setFollow] = useState(false);
   console.log(id);
   useEffect(() => {
     // request from the back on the follow status
-  });
+    (async () => {
+      const resp = await getFollowing(id);
+      setFollow(resp.data.data.following);
+    })();
+  }, []);
 
   // <button className={styles.wrapper} type="button" onCklick={handleOpenProfile}>
   return (
@@ -47,22 +52,35 @@ function SearchUser({
       <div className={styles.username}>
 
         {username}
+        {localStorage.userId !== id
+        && (
         <button
           className={styles[(follow) ? buttonStyleClicked : buttonStyle]}
           type="button"
           id="followbutton"
+          disabled={localStorage.userId === id}
           onClick={() => {
             if (follow === true) {
               // request and unfollow
+              unFollow(id).then((response) => {
+                if (response.status === 200) {
+                  setFollow(!follow);
+                }
+              });
             } else {
               // request and follow
+              Follow(id).then((response) => {
+                if (response.status === 200) {
+                  setFollow(!follow);
+                }
+              });
             }
             setFollow(!follow);
-            setIsButtonClicked(!isButtonClicked);
           }}
         >
           <span />
         </button>
+        )}
       </div>
 
       <div>
@@ -89,13 +107,11 @@ SearchUser.propTypes = {
   profilePic: PropTypes.string.isRequired,
   buttonStyle: PropTypes.string,
   buttonStyleClicked: PropTypes.string,
-  isButtonActive: PropTypes.bool,
 };
 
 SearchUser.defaultProps = {
   buttonStyle: 'tweetfollowbutton',
   buttonStyleClicked: 'tweetfollowingbutton',
-  isButtonActive: false,
 };
 
 export default SearchUser;
